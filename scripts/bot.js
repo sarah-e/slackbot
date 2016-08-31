da602f02596405920d125f2fe1f8dcb7c2d16089
Sarah Esteban <sarah@viator.com>
2016-08-31 19:50:54 +1000

    add regular expression

--- scripts/bot.js
+++ scripts/bot.js
@@ -46,4 +46,19 @@ module.exports = function (robot) {
 
     }
   })
+
+  robot.respond(/is it a (weekend|holiday)\s?\?/i), function(msg){
+    let today = new Date();
+
+    msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+  });
+
+  robot.hear(/I did it/i, function(msg){
+    msg.send("Congratulations! Good job!");
+  });
+
+  robot.respond(/convert \$(.*) to btc/i, function(res){
+    let usd = res.match[1];
+    res.reply("That is about " + usd*0.0024 + " in BTC");
+  });
 }
