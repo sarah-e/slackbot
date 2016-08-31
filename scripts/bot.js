ac52b06ba265b2e394f9b94f4f970e722e3d26de
Sarah Esteban <sarah@viator.com>
2016-08-31 18:58:57 +1000

    add food

--- scripts/bot.js
+++ scripts/bot.js
@@ -2,4 +2,7 @@ module.exports = function (robot) {
   robot.hear(/Hello!/, function(res){
     return res.send("Hi there!");
   })
+  robot.respond(/What's your favourite food?/, function(res){
+    return res.send("I'm a robot--I don't eat food!");
+  })
 }
