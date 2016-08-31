0a8f333d041677c0cf804d5aedb271de03fd5efd
Sarah Esteban <sarah@viator.com>
2016-08-31 19:52:47 +1000

    fix error

--- scripts/bot.js
+++ scripts/bot.js
@@ -47,7 +47,7 @@ module.exports = function (robot) {
     }
   })
 
-  robot.respond(/is it a (weekend|holiday)\s?\?/i), function(msg){
+  robot.respond(/is it a (weekend|holiday)\s?\?/i, function(msg){
     let today = new Date();
 
     msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
